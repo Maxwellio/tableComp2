71b03437023d967de654970af769416f7224c2a8
Maxwellio <pfcmax@mail.ru>
2026-07-30 14:47:49 +0500

    Update and rename index.ts to index.d.ts

---
+++ mainComponent/index.d.ts
@@ -0,0 +1,9 @@
+export { BaseTable } from './src/BaseTable/BaseTable';
+export { BaseTreeTable } from './src/BaseTable/BaseTreeTable';
+export { ChangePasswordInput, CheckBox, DebouncedInput, DebouncedParamInput, DynamicDateList, DynamicDatePicker, DynamicParamSelect, DynamicSelect, ParameterSelector, PasswordInput, RangePicker, ReportSign, SwitchImput } from './src/Input/InputComponents';
+export { Navigation } from './src/Navigation/Navigation';
+export { AxiosProvider, useApi } from './src/services/api';
+export { ToastProvider, useToast } from './src/Toast/ToastContext';
+export { formatDate, getMonthName } from './src/utils/dateFormatter';
+export { FILTER_TYPES } from './src/utils/types';
+
