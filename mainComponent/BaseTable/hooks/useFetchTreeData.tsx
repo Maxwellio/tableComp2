05d71c22ca37540c17e48e9a1c7064bd7bf89c42
Maxwellio <pfcmax@mail.ru>
2026-08-21 14:59:31 +0500

    Update useFetchTreeData.tsx

--- mainComponent/BaseTable/hooks/useFetchTreeData.tsx
+++ mainComponent/BaseTable/hooks/useFetchTreeData.tsx
@@ -1,19 +1,28 @@
 import { useCallback, useEffect, useState } from 'react';
 import { useApi } from '../../services/api';
 
-export const useFetchData = <T,> (url:String, id:Number) => {
-    const api = useApi(); //обязательно использовать <AxiosProvider>
+export const useFetchData = <T,> (url:String, columnFilters: Record<string,any>, reRenderSignal:any) => {
+    const api = useApi();
     const [data, setData] = useState<T[]>([]);
     const [loading, setLoading] = useState(false);
     const [error, setError] = useState(null);
- 
+    
     useEffect(() => {
-        const fetchData = async () => {
+        const fetchData = async (filters) => {
+            const newfilers = filters.reduce((acc, filter) => {
+                if (filter.value !== undefined && filter.value !== null && String(filter.value).trim() !== '') {
+                    acc[filter.id] = filter.value;
+                }
+                return acc;
+            }, {});
             setLoading(true);
             try {
-                const reqApi = `${url}/${id}`;
-                const data = (await api.get(reqApi)).data;
-                setData(Array.of(data));
+                const params = {
+                    ...newfilers
+                }
+                const reqApi = String(url);
+                const data = (await api.get(reqApi, {params})).data;
+                setData(data);
                 setError(null);
             } catch(err){
                 setError(err.message);
@@ -21,21 +30,24 @@ export const useFetchData = <T,> (url:String, id:Number) => {
                 setLoading(false);
             }
         };
-        fetchData();
-    }, []);
+        fetchData(columnFilters);
+    }, [JSON.stringify(columnFilters), reRenderSignal]);
 
     const fetchChildren = useCallback(async (parentId,) => {
         const reqApi = `${url}/${parentId}/children`;
         const children = (await api.get(reqApi)).data;
         setData(prev => {
             const updateNode = (nodes) =>{
-                return nodes.map(node => {         
+                return nodes.map(node => {
+                    // Если узел родительский - вставляем
                     if (node.id  === parentId && !node.hasLoaded){
                         return {...node, children: children, hasLoaded: true};
                     }
+                    // Обходим дерево в поиске родителя
                     if (node.children) {
-                        return {...node, children: updateNode(node.children), hasLoaded: true}
+                        return {...node, children: updateNode(node.children)}
                     }
+                    // Возврат узла если дошли до конца и не нашли куда вставить
                     return node;
                 })
             }
@@ -51,5 +63,4 @@ export const useFetchData = <T,> (url:String, id:Number) => {
         fetchChildren
     }
 };
-
 
